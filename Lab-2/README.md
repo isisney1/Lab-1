6477592bbc8c0e285a713776b58916fe0034e2e7
isisney1 <isisney1@umbc.edu>
2026-09-10 16:26:52 +0000

    Refactor Task 3 assembly code to use indirect addressing for RAM storage and update README with Task 3 compile/run instructions

--- Lab-2/README.md
+++ Lab-2/README.md
@@ -3,7 +3,9 @@
 gcc -nostdlib -no-pie print_hello.s -o hello
 <br>gcc -no-pie Task_1.s print_ram.c -o task1
 <br>gcc -no-pie Task_2.s print_ram.c -o task2
+<br>gcc -no-pie Task_3.s print_ram.c -o task3
 ## Run
 ./hello
 <br>./task1
 <br>./task2
+<br>./task3
--- Lab-2/Task_3.s
+++ Lab-2/Task_3.s
@@ -6,17 +6,15 @@
 .global fill_ram            # Make function visible to C program
 
 fill_ram:
-    #Store FFh into RAM locations 50H - 58H using direct addressing
+    #Store FFh into RAM locations 50H - 58H using indirect addressing
 
-    movb $0x00, ram+0x50
-    movb $0x00, ram+0x51
-    movb $0x00, ram+0x52
-    movb $0x00, ram+0x53
-    movb $0x00, ram+0x54
-    movb $0x00, ram+0x55
-    movb $0x00, ram+0x56
-    movb $0x00, ram+0x57
-    movb $0x00, ram+0x58
+    movl $(ram + 0x50), %eax
+
+    ram_loop:
+        movb $0x00, (%eax)
+        incl %eax
+        cmpl $(ram + 0x59), %eax
+        jne ram_loop
 
     ret                     # Return control back to C program
 
