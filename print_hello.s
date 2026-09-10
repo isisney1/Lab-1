6fd7f98d7a69292cbbeed57cb0d1534788460aa4
isisney1 <isisney1@umbc.edu>
2026-09-10 14:26:40 +0000

    Add Task 2 assembly file and improve comments in print_hello.s

--- print_hello.s
+++ print_hello.s
@@ -5,12 +5,12 @@ len = . - msg
 .section .text
 .global _start
 _start:
-    mov $1,  %rax
-    mov $1,  %rdi
-    mov $msg,%rsi
-    mov $len,%rdx
+    mov $1,  %rax   # write
+    mov $1,  %rdi   # stdout
+    mov $msg,%rsi   # buf
+    mov $len,%rdx   # len
     syscall
 
-    mov $60, %rax
-    mov $0,  %rdi
+    mov $60, %rax   # exit
+    mov $0,  %rdi   # status
     syscall
